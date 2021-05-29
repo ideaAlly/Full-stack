96b03560d53ff5fc025068843f71a94fb87a0839
shamanth a r <shamanthar2000@gmail.com>
2021-05-29 09:18:19 +0530

    Update dated 29/05/2021

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import LoginPage from './components/LoginPage/LoginPage';
 import './components/LoginPage/LoginPage.css';
+import Profile from './components/Profile/Profile';
 // import Box from './components/InterestsPage/Box.js';
 // import './components/InterestsPage/Box.css';
 // import Navbar from './components/InterestsPage/Navbar.js';
@@ -8,7 +9,7 @@ import './components/LoginPage/LoginPage.css';
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Profile/>
     </div>
   );
 }
---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+
+import { BsPeopleCircle } from "react-icons/bs"
+import { BsPersonFill } from "react-icons/bs";
+import {BsEnvelopeFill } from "react-icons/bs";
+import { BsPhone } from "react-icons/bs";
+import { BsBuilding} from "react-icons/bs";
+import {BsHeartFill } from "react-icons/bs";
+import './Profile.css';
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
+const Profile = () => {
+    const [ModalIsOpen,setModalIsOpen] = useState(false);
+    return(
+        <>
+            <div className="Card">
+                <div className="Upper-Container">
+                    <div className="Image-Container">
+                        <div className="Image"><BsPeopleCircle size="5em"/></div>
+                    </div>
+                </div>
+                <div className="Lower-Container">
+                    <div>
+                        <h3 className="Name"><BsPersonFill/>&nbsp;Shamanth A R</h3>
+                        <h4 className="Email"><BsEnvelopeFill/>&nbsp;shamanthar2000@gmail.com</h4>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className="Phone"><BsPhone/> +91 720 439 1409</li>
+                            <li className="Bio"><BsBuilding/> Student at NMAMIT.</li>
+                            <li className="Interests"><BsHeartFill/> Web Development, Artificial Intelligence</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <button className="Btn" onClick={() => {
+							setModalIsOpen(true)	;
+                        }}>Edit Profile</button>
+
+                        
+                        <Modal
+                            isOpen={ModalIsOpen}
+                            onRequestClose={() => setModalIsOpen(false)}
+                            style={{
+                                overlay: {
+                                    
+                                    background:'grey'
+                                },
+                                content: {
+                                    color: 'cadetblue',
+                                    background:'black'
+                                }
+                            }}
+                        >
+
+                            <div className="ModalCard">
+                                <div className="ModalUpper">
+                                   <BsPeopleCircle size="8em"/>
+                                </div>
+                                <div className="ModalLower">
+                                    <ul className="ModalContents">
+                                        <li className="ModalList">
+                                            Name:   <input type="text"></input>
+                                        </li >
+
+                                        <li className="ModalList" >
+                                            Emai:   <input type="email" width="50px"></input>
+                                        </li>
+
+                                        <li className="ModalList">
+                                            Contact Number:   <input type="text"></input>
+                                        </li>
+
+                                        <li className="ModalList">
+                                            Bio:   <textarea></textarea>
+                                        </li>
+
+                                        <li className="ModalList">
+                                            Interests:   <input type="text"></input>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="ModalLowerButtons">
+                                    <button className="Submit">Save</button>
+                                    <button className="Cancel" onClick={() => {
+							setModalIsOpen(false)	;
+                        }}>Cancel</button>
+                                </div>
+                            </div>
+
+                            
+                        </Modal>
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Profile;
